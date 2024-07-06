a030dd69f76c7ae6de123f5b27c400460beecc1c
NikshayMohla <78416149+NikshayMohla@users.noreply.github.com>
2024-07-06 14:54:19 +0530

    UI HTML

--- script.js
+++ script.js
@@ -1,12 +1,72 @@
 const key="dd1bdb5745504f8ebf8145338240507"
-async function fetchWeatherDetails(){
-    let city='Chandigarh'
-    const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`;
 
-    const response=await fetch(url);
-    const data= await response.json();
-    console.log(data);
-    let newpara = document.createElement('p');
-    newpara.textContent= `${data?.current?.temp_c.toFixed(2)} °C`;
-    document.body.appendChild(newpara);
+function renderWeatherInfo(data) {
+    let newPara = document.createElement('p');
+    newPara.textContent = `${data?.current?.temp_c.toFixed(2)} °C`
+
+    document.body.appendChild(newPara);
 }
+
+
+async function fetchWeatherDetails() {
+
+    try {
+        let city = "chandigarh";
+
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`);
+        const data = await response.json();
+    
+        console.log("Weather data:-> " , data);
+
+        renderWeatherInfo(data);
+    }
+    catch(err) {
+        //handle the error here
+    }
+
+}
+
+
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else console.log("Please provide location access");
+}
+
+function showPosition(position)
+{
+    let lat=position.coords.latitude;
+    let long=position.coords.longitude;
+
+    console.log(lat);
+    console.log(long);
+}
+
+
+
+
+function switchTab(clickedTab) {
+
+    apiErrorContainer.classList.remove("active");
+  
+    if (clickedTab !== currentTab) {
+      currentTab.classList.remove("current-tab");
+      currentTab = clickedTab;
+      currentTab.classList.add("current-tab");
+  
+      if (!searchForm.classList.contains("active")) {
+        userInfoContainer.classList.remove("active");
+        grantAccessContainer.classList.remove("active");
+        searchForm.classList.add("active");
+      } 
+      else {
+        searchForm.classList.remove("active");
+        userInfoContainer.classList.remove("active");
+        //getFromSessionStorage();
+      }
+  
+      // console.log("Current Tab", currentTab);
+    }
+  }
+  
